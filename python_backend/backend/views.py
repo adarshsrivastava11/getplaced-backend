030fe1398a266cd662d09151bc39036381e9d7ef
Adarsh <adarshs@iitk.ac.in>
2019-04-30 14:46:33 +0530

    Django REST Framework Added

--- python_backend/backend/views.py
+++ python_backend/backend/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.response import Response
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
+@api_view()
+@authentication_classes((SessionAuthentication, BasicAuthentication))
+@permission_classes((IsAuthenticated,))
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    print (request.user)
+    return Response({"message":"Hello, world. You're at the polls index."})
